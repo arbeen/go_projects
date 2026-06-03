f99526c4a52d86d28db4643fe046f3d78f44d15a
arbeen <arbinshrestha98@gmail.com>
2026-06-02 23:27:35 -0400

    initializes jsontodo

---
+++ jsontodo/go.mod
@@ -0,0 +1,3 @@
+module example.com/jsontodo
+
+go 1.26.3
---
+++ jsontodo/jsontodo.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Todo struct {
+	ID   int
+	Text string
+	Done bool
+}
+
+func addTodo(todo string) error {
+	return nil
+
+}
+
+func main() {
+	fmt.Println("Choose an action:")
+	fmt.Println("1. Add a todo")
+	fmt.Println("2. Mark todo done")
+	fmt.Println("3. List all todos")
+	fmt.Println("4. Delete a todo")
+	fmt.Println("*. Exit")
+	var action string
+	fmt.Scan(&action)
+
+	switch action {
+	case "1":
+		//add a todo
+		fmt.Println("Enter todo: ")
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			if err := addTodo(scanner.Text()); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
